fa0650af4dd91f489abdbabf9c370b8bda55e6d0
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-13 12:45:15 -0300

    estado, flatlist e empty

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,38 +1,42 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { styles } from "./styles";
 import Header from "../components/Header";
 import Task from "../components/Task";
+import { useState } from "react";
+import { TaskDTO } from "../dtos/TaskDTO";
+import Empty from "../components/Empty";
 
 const HomeScreen = () => {
+  const [tasks, setTasks] = useState<TaskDTO[]>([]);
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.tasksContainer}>
         <View style={styles.info}>
           <View style={styles.row}>
-          <Text style={styles.tasksCreated}>Criadas</Text>
-          <View style={styles.counterContainer}>
-            <Text style={styles.counterText}>0</Text>
-          </View>
+            <Text style={styles.tasksCreated}>Criadas</Text>
+            <View style={styles.counterContainer}>
+              <Text style={styles.counterText}>0</Text>
+            </View>
           </View>
           <View style={styles.row}>
-          <Text style={styles.tasksDone}>Concluídas</Text>
-          <View style={styles.counterContainer}>
-            <Text style={styles.counterText}>0</Text>
-          </View>
+            <Text style={styles.tasksDone}>Concluídas</Text>
+            <View style={styles.counterContainer}>
+              <Text style={styles.counterText}>0</Text>
+            </View>
           </View>
         </View>
-        <Task 
-        title="Estudar Mobile" 
-        isCompleted={false} 
-        />
-        <Task 
-        title="Estudar Mobile" 
-        isCompleted
-        />
-        <Task 
-        title="Estudar Mobile" 
-        isCompleted
+        <FlatList
+          data={tasks}
+          keyExtractor={(tasks) => tasks.id!}
+          renderItem={({ item }) => (
+            <Task
+              key={item.id}
+              isCompleted={item.isCompleted}
+              title={item.title}                     
+                />
+          )}
+          ListEmptyComponent={<Empty />}
         />
       </View>
     </View>
