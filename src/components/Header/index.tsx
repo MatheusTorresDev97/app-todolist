e0f52c670f8e58c0afb15fab5320e538e66d7796
Matheus <matheustorresdev@gmail.com>
2023-12-13 20:48:44 -0300

    adicionando tarefas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,13 @@ import logoImage from "../../assets/logo.png"
 import { theme } from "../../theme"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 
-const Header = () => {
+type HeaderProps = {
+  task: string,
+  onChangeText: (task: string) => void
+  onPress: () => void
+}
+
+const Header = ({task, onChangeText, onPress}: HeaderProps) => {
   return (
     <View style={styles.headerContainer}>
         <Image source={logoImage} />
@@ -13,8 +19,10 @@ const Header = () => {
           style={styles.input}
            placeholder="Adicione uma nova Tarefa"
            placeholderTextColor={theme.colors.base.gray300}
+           value={task}
+           onChangeText={onChangeText}
            />
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={onPress}>
             <MaterialCommunityIcons name="plus-circle-outline" size={24} color={theme.colors.base.gray100}/>
              </TouchableOpacity>
         </View>
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -5,12 +5,30 @@ import Task from "../components/Task";
 import { useState } from "react";
 import { TaskDTO } from "../dtos/TaskDTO";
 import Empty from "../components/Empty";
+import { uuid } from "../utils/uuid";
 
 const HomeScreen = () => {
   const [tasks, setTasks] = useState<TaskDTO[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleTaskAdd = () => {
+    if (newTask !== "" && newTask.length >= 5) {
+      setTasks((tasks) => [
+        ...tasks,
+        { id: uuid(), isCompleted: false, title: newTask.trim() },
+      ]);
+
+      setNewTask('')
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header
+        task={newTask}
+        onChangeText={setNewTask}
+        onPress={handleTaskAdd}
+      />
       <View style={styles.tasksContainer}>
         <View style={styles.info}>
           <View style={styles.row}>
@@ -33,8 +51,8 @@ const HomeScreen = () => {
             <Task
               key={item.id}
               isCompleted={item.isCompleted}
-              title={item.title}                     
-                />
+              title={item.title}
+            />
           )}
           ListEmptyComponent={<Empty />}
         />
