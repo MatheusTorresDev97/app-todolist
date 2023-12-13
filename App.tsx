acff07ac9aae10e865dd1d0cc0feb81f912f51b0
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-13 11:26:02 -0300

    estrutura inicial e configurando fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_700Bold,
+} from '@expo-google-fonts/inter'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold
+  })
+
   return (
     <View style={styles.container}>
+      {fontsLoaded ? <Text>Carregou...</Text> : <ActivityIndicator size={50} /> }
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
