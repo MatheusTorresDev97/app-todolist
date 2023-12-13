cead99ee1accac9b33f722364e4d10da93fec1c5
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-13 11:41:40 -0300

    criando tema, screen home e loading

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import {
 	Inter_400Regular,
 	Inter_700Bold,
 } from '@expo-google-fonts/inter'
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { theme } from './src/theme';
+import HomeScreen from './src/screens';
+import Loading from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,19 +16,10 @@ export default function App() {
   })
 
   return (
-    <View style={styles.container}>
-      {fontsLoaded ? <Text>Carregou...</Text> : <ActivityIndicator size={50} /> }
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      {fontsLoaded ? <HomeScreen /> : <Loading /> }
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
